c4af25fe7340f6a3419b8a51ff82377aed175773
ithodol <ddmdjesz1@gmail.com>
2025-07-07 02:04:50 +0900

    7/6 수정 페이지 수정 완료

--- user/update.php
+++ user/update.php
@@ -18,22 +18,30 @@ $user = $result->fetch_assoc();
 
 <?php include '../header.php'; ?>
 
-<h2>👤 회원 정보 수정</h2>
-<form action="updatePro.php" method="post">
-    <input type="hidden" name="uno" value="<?php echo $uno; ?>">
+<div class="userEditCon">
+  <h2 class="editTitle">👤 회원 정보 수정</h2>
+  <form class="userEditForm" action="updatePro.php" method="post">
+      <input type="hidden" name="uno" value="<?= $uno ?>">
 
-    <p>아이디: <strong><?php echo htmlspecialchars($user['uid']); ?></strong></p>
+      <p>아이디 <strong><?= htmlspecialchars($user['uid']) ?></strong></p>
 
-    <p>이름: <input type="text" name="uname" value="<?php echo htmlspecialchars($user['uname']); ?>" required></p>
+      <p>이름
+          <input type="text" name="uname" value="<?= htmlspecialchars($user['uname']) ?>" required>
+      </p>
 
-    <p>전화번호: <input type="text" name="uphone" value="<?php echo htmlspecialchars($user['uphone']); ?>" required></p>
+      <p>전화번호
+          <input type="text" name="uphone" value="<?= htmlspecialchars($user['uphone']) ?>" required>
+      </p>
 
-    <p>새 비밀번호: <input type="password" name="upwd" placeholder="변경 시에만 입력"></p>
+      <p>새 비밀번호
+          <input type="password" name="upwd" placeholder="변경 시에만 입력">
+      </p>
 
-    <button type="submit">수정 완료</button>
-    <button type="button"><a href="info.php">취소</a></button>
-    
-</form>
+      <button type="submit">수정 완료</button>
+      <button type="button"><a href="info.php">취소</a></button>
+  </form>
+</div>
+
 
 </body>
 </html>
